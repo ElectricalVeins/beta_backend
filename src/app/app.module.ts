28efdd3bf0a9d8a6c6284ceafd7d4cb39e1557b1
vozir <maxmost512@gmail.com>
2022-06-01 16:54:02 +0300

    add db connect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configuration from '../config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,6 +11,19 @@ import configuration from '../config/configuration';
       load: [configuration],
       cache: true,
       ignoreEnvFile: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('db.host'),
+        port: configService.get('db.port'),
+        username: configService.get('db.user'),
+        password: configService.get('db.password'),
+        database: configService.get('db.name'),
+        autoLoadEntities: true,
+      }),
     }),
   ],
   controllers: [AppController],
