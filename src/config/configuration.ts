903ccad7c8cae4c8a20b32479cc1cba0f2dfcb07
vozir <maxmost512@gmail.com>
2022-06-05 17:50:28 +0300

    Change config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,18 +1,23 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-const CONFIG_PATH = join(__dirname, '..', '..', 'config');
-
 const map = {
-  default: 'default.json',
-  development: 'dev.json',
-  production: 'default.json',
-  test: 'test.json',
+  default: 'default',
+  development: 'dev',
+  production: 'default',
+  test: 'test',
 };
 /* TODO: make deafult config to be a basis for other configs */
 export default () => {
   const configKey = process.env.NODE_ENV?.toLowerCase() || 'development';
-  const configFilePath = join(CONFIG_PATH, map[configKey]);
+  const configFilePath = join(
+    __dirname,
+    '..',
+    '..',
+    'config',
+    map[configKey],
+    '.json'
+  );
   const configString = readFileSync(configFilePath, 'utf8');
   const config = JSON.parse(configString);
   console.log(
