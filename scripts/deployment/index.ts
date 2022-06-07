2a7d1a26aa3c18ec48a09269180d3a0c890904c0
vozir <maxmost512@gmail.com>
2022-06-07 19:46:14 +0300

    Add env bootstrap

---
+++ scripts/deployment/index.ts
@@ -0,0 +1,24 @@
+import { spawn } from 'child_process';
+
+export async function bashCommand(
+  command: string,
+  args?: string[],
+  errorMessage?: string
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const cli = spawn(command, args, { shell: true, stdio: 'inherit' });
+
+    // cli.stdout.on('data', (data) => { console.log(data.toString()) })
+    // cli.stderr.on('data', (data) => { console.log(data.toString()) })
+
+    cli.on('exit', (code) => {
+      if (code > 0) {
+        reject(errorMessage);
+      }
+      resolve();
+    });
+    cli.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
---
+++ scripts/deployment/local/index.ts
@@ -0,0 +1,28 @@
+import { bashCommand } from '..';
+
+async function run(command: string) {
+  switch (command.toLowerCase()) {
+    case 'start': {
+      return await bashCommand('docker-compose', [
+        '-f',
+        `${__dirname}/docker-compose.yaml`,
+        'up',
+        '-d',
+      ]);
+    }
+    case 'stop': {
+      return await bashCommand('docker-compose', [
+        '-f',
+        `${__dirname}/docker-compose.yaml`,
+        'stop',
+      ]);
+    }
+    default: {
+      throw new Error('Missing script argument');
+    }
+  }
+}
+
+run(process.argv.slice(2)[0])
+  .then(() => console.log('Successfully prepared'))
+  .catch(() => console.log('Check env dependencies in readme file'));
