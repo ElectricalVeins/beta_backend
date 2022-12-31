37565330f05d46518d83e22ee0e0da6721c972cd
ElectricalVeins <maxmost512@gmail.com>
2022-12-31 15:14:56 +0200

    Add health check

--- src/app/app.controller.spec.ts
+++ src/app/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.ping()).toBe('ok!');
+      expect(appController.healthCheck()).toBe('ok');
     });
   });
 });
