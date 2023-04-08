9f659cae7ae044ea606f75c651a4c3f4031dd6c0
ElectricalVeins <maxmost512@gmail.com>
2023-04-09 01:58:40 +0300

    Revise auth approach

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,45 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseGuards,
+  Req,
+  NotImplementedException,
+  Delete,
+  Param,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Public } from '../utils/decorator/Public';
 import { AuthService } from './auth.service';
+import { User } from '../user/user.entity';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginUserDto } from '../user/dto/login-user.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { CurrentUser } from '../utils/decorator/CurrentUser';
+import { JwtPayload, UserAuth } from '../types';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Public()
   @Post('signup')
-  async signUp(@Body() dto: CreateUserDto): Promise<any> {
-    return this.authService.signUp(dto);
+  async signUp(@Body() dto: CreateUserDto, @Req() req: Request): Promise<UserAuth> {
+    const userAgent = req.headers['user-agent'];
+
+    return this.authService.signUp(dto, userAgent);
   }
 
   @Public()
   @Post('signin')
-  async signIn(@Body() dto: LoginUserDto): Promise<any> {
-    return this.authService.signIn(dto);
+  @ApiOperation({ description: 'Email could also be used as a login' })
+  async signIn(@Body() dto: LoginUserDto, @Req() req: Request): Promise<UserAuth> {
+    const userAgent = req.headers['user-agent'];
+
+    return this.authService.signIn(dto, userAgent);
   }
 
   @Public()
@@ -30,8 +50,10 @@ export class AuthController {
     summary: 'Refresh authentication',
     description: 'Send refresh token to get the new token couple',
   })
-  async refresh(@Query('auth') refreshToken: string): Promise<any> {
-    return this.authService.refreshSession(refreshToken);
+  async refresh(@Query('token') refreshToken: string, @Req() req: Request): Promise<UserAuth> {
+    const userAgent = req.headers['user-agent'];
+
+    return this.authService.refreshSession(refreshToken, userAgent);
   }
 
   @Get('activate')
@@ -41,7 +63,32 @@ export class AuthController {
     summary: 'Account activation',
     description: 'Send token from email to activate account',
   })
-  async activate(@Query('token') token: string): Promise<any> {
+  async activate(@Query('token') token: string): Promise<Partial<User>> {
     return this.authService.activateUser(token);
+  }
+
+  @Get('existing-sessions')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Get existing sessions',
+    description: 'Get existing session info. Currently returns list of signed tokens for user',
+  })
+  async existingSessions(@CurrentUser() user: JwtPayload): Promise<object[]> {
+    return this.authService.getExistingSessions(Number(user.userid));
+  }
+
+  @Delete('existing-sessions/:sessionId')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: 'Sign out from existing session',
+    description:
+      'Sign out from existing session. This marks current token as deprecated and forbids to use it as authentication credential. You need to pass the session id, which is available in /existing-sessions endpoint',
+    parameters: [{ name: 'sessionId', in: 'path' }],
+  })
+  async signOut(@CurrentUser() user: JwtPayload, @Param('sessionId') sessionId: string): Promise<string> {
+    // TODO: AUTH | store deleted session until token expires in blacklist.
+    throw new NotImplementedException();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { Cache } from 'cache-manager';
 import { TokenService } from '../jwt-token/jwt-token.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserService } from '../user/user.service';
 import { RefreshTokenService } from '../token-refresh/token-refresh.service';
 import { MailService } from '../mail/mail.service';
 import { JwtPayload, JwtTokenTypes, UserAuth } from '../types';
+import { LoginUserDto } from '../user/dto/login-user.dto';
+import { User } from '../user/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -14,31 +17,33 @@ export class AuthService {
     private readonly tokenService: TokenService,
     private readonly userService: UserService,
     private readonly refreshTokenService: RefreshTokenService,
-    private readonly mailService: MailService
+    private readonly mailService: MailService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
   ) {}
 
-  public async signUp(dto: CreateUserDto): Promise<UserAuth> {
+  public async signUp(dto: CreateUserDto, userAgent: string): Promise<UserAuth> {
     const user = await this.userService.create(dto);
     const { id: tier } = await user.tier;
-    const [[access, refresh], setTokenCache] = await this.tokenService.signTokens({
-      userid: user.id,
-      role: user.role['id'],
-      tier: tier.toString(),
-    });
-    await this.refreshTokenService.createRecord(refresh, user.id, false);
+    const { access, refresh, id } = await this.tokenService.signTokens(
+      {
+        userid: user.id,
+        role: user.role['id'],
+        tier: tier.toString(),
+      },
+      userAgent
+    );
     try {
       this.mailService.sendConfirmationEmail(user);
     } catch (e) {
       Logger.error(e);
     }
-    setTokenCache();
     return {
       user,
-      tokens: { access, refresh },
+      session: { access, refresh, id },
     };
   }
 
-  public async signIn(dto): Promise<UserAuth> {
+  public async signIn(dto: LoginUserDto, userAgent: string): Promise<UserAuth> {
     const user = await this.userService.findOneByLoginOrEmail(dto.login);
     if (!user) {
       throw new UnauthorizedException('Incorrect login or password');
@@ -48,47 +53,51 @@ export class AuthService {
     if (!isCorrectPassword) {
       throw new UnauthorizedException('Incorrect login or password');
     }
-    const [[access, refresh], setTokenCache] = await this.tokenService.signTokens({
-      userid: user.id,
-      role: user.role['id'],
-      tier: tier.toString(),
-    });
-    await this.refreshTokenService.createRecord(refresh, user.id);
-    setTokenCache();
+    const { access, refresh, id } = await this.tokenService.signTokens(
+      {
+        userid: user.id,
+        role: user.role['id'],
+        tier: tier.toString(),
+      },
+      userAgent
+    );
+
     return {
       user,
-      tokens: { access, refresh },
+      session: { access, refresh, id },
     };
   }
 
-  public async activateUser(token: string): Promise<any> {
+  public async activateUser(token: string): Promise<Partial<User>> {
     const { userid, tier }: Partial<JwtPayload> = await this.tokenService.verifyConfirmEmailToken(token);
     return await this.userService.activateUser(userid, tier);
   }
 
-  public async refreshSession(refreshToken: string): Promise<UserAuth> {
+  public async refreshSession(refreshToken: string, userAgent: string): Promise<UserAuth> {
     const { id, tier } = await this.tokenService.verifyToken(refreshToken, JwtTokenTypes.REFRESH);
     const user = await this.userService.findOneById(id, tier);
     if (!user) {
       throw new NotFoundException();
     }
-    const [[access, refresh], setTokenCache] = await this.tokenService.signTokens({
-      userid: user.id,
-      role: user.role['id'],
-      tier: tier.toString(),
-    });
-    const existingRefreshToken = await this.refreshTokenService.getTokenRecordByValue(refreshToken, user.id);
-    /*TODO: use of transactionalEntityManager IS REQUIRED */
-    await this.dataSource.transaction(async () =>
-      Promise.all([
-        this.refreshTokenService.deleteRecord(existingRefreshToken),
-        this.refreshTokenService.createRecord(refresh, user.id),
-      ])
+    const { access, refresh } = await this.tokenService.signTokens(
+      {
+        userid: user.id,
+        role: user.role['id'],
+        tier: tier.toString(),
+      },
+      userAgent
     );
-    setTokenCache();
+
     return {
       user,
-      tokens: { access, refresh },
+      session: { access, refresh, id },
     };
+  }
+
+  public async getExistingSessions(userId: number): Promise<object[]> {
+    const keys = await this.cacheManager.store.keys(this.tokenService.createTokenKey(String(userId), '*'));
+    const tokens = await Promise.all<object>(keys.map(async (key) => this.cacheManager.get<object>(key)));
+    // TODO: return session info, instead of token list
+    return tokens;
   }
 }
